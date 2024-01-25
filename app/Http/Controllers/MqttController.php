535d2d26104f1305ac2693d0d1c41d585b53cd11
Hope-saya <hope.saya@strathmore.edu>
2024-01-25 17:37:14 +0300

    savanatrace-app

--- app/Http/Controllers/MqttController.php
+++ app/Http/Controllers/MqttController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use PhpMqtt\Client\Facades\MQTT;
+use Illuminate\Support\Facades\Log;
 
 class MqttController extends Controller
 {
-    //
+    public function updateIpAddress()
+    {
+        // Defining the variables
+        $brokerHost = 'broker.emqx.io';
+        $brokerPort = 1883;
+        $topic = 'ip-updates';
+
+        try {
+            // Connecting to MQTT broker
+              //initializing an instance of the MQTT client.
+            $mqtt = MQTT::connection();
+
+            $mqtt->connect($brokerHost, $brokerPort);
+
+            // Reading the current IP address
+            $ipAddress = $this->getIpAddress();
+
+            // Publishing IP address to the specified topic
+            $mqtt->publish($topic, $ipAddress);
+
+            // Disconnect from MQTT broker
+            $mqtt->disconnect();
+
+            Log::info('success!.');
+
+            // Return a JSON response indicating success
+            return response()->json(['message' => 'IP Address updated and broadcasted successfully']);
+        } catch (\Exception $e) {
+            // Log error message if MQTT update fails
+            Log::error('MQTT Update Failed: ' . $e->getMessage());
+
+            // Return a JSON response indicating failure
+            return response()->json(['error' => 'Failed to update IP address via MQTT']);
+        }
+    }
+
+    private function getIpAddress()
+    {
+        // retrieving the current IP addresss
+        
+    
+    }
 }
+
